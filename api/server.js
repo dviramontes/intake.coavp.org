921ebe1605d7a932a7c3fed3a7cdf474060efbaa
david viramontes <dviramontes@gmail.com>
2014-02-03 00:43:45 -0700

    adds GET plural route

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@
 //	server config
 
 var restify = require('restify'),
+    // all users are deleted after server restart
+    // save, the module, uses memory persistance by default
     userSave = require('save')('user');
 
 var server = restify.createServer({
@@ -13,7 +15,7 @@ server
     .use(restify.fullResponse())
     .use(restify.bodyParser());
 
-
+//  GET plural
 //  curl -i http://0.0.0.0:9000/user |  json
 server.get('/user', function(req, res, next) {
 
@@ -23,8 +25,24 @@ server.get('/user', function(req, res, next) {
 
 });
 
-// all users are deleted after server restart
+// GET single
+server.get('/user/:id', function(req, res, next) {
 
+    userSave.findOne({
+        _id: req.params.id
+    }, function(err, user) {
+        if (err) return next(new restify.InvalidArgumentError(JSON.stringify(err.errors)));
+        if (user) {
+            res.send(user)
+        } else {
+            res.send(404);
+        }
+
+    })
+})
+
+
+// POST single
 server.post('/user', function(req, res, next) {
 
     if (req.params.name === undefined) {
