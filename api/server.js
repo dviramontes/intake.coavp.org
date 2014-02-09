d85bef9930b03ecf189477649763205bc4ab0856
dviramontes <dviramontes@gmail.com>
2014-02-09 13:43:56 -0700

    adds delete route.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
-//  restify
-//              --API--
-//  server config
+/*====================================
+
+	TODO:
+	[   ]  -  put / update route
+	[   ]  -  delete route
+	[--]  -  get / single  by caseNumber
+	[X]  -  post / single
+
+====================================*/
 
 'use strict';
 
@@ -9,17 +15,11 @@ var restify = require('restify'),
 	mongoose = require('mongoose'),
 	Intake,
 	testIntakeInsert;
-// all users are deleted after server restart
-// save, the module, uses  in-memory persistance by default
-// save = require('save'),
-// s = require('Intake'),
-// use save-mongodb for real persistance
-// saveMongodb = require('save-mongodb'),
-
 
 mongoose.connect('mongodb://127.0.0.1:27017/intakes');
 var db = mongoose.connection;
-Intake = require('../models/Intake').Intake;
+Intake = require('../models/Intake')
+	.Intake;
 // call to disconnect ,,,
 // mongoose.disconnect();
 
@@ -27,7 +27,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 console.log('connection success'.green);
 
 db.on('open', function callback() {
-	testIntakeInsert();
+	// testIntakeInsert();
 	console.log('database opened'.green);
 });
 
@@ -41,44 +41,39 @@ server
 
 //  GET plural
 //  curl -i http://0.0.0.0:9000/user |  json
-server.get('/intakes', function (req, res) {
-	// run in robomongo shell or mongo shell
-	// to see how the query is ran
-	// db.intakes.find().explain();
+server.get('/intakes', function(req, res) {
 
-	// lets find some intakes..
-	Intake.find({}, function (err, foundIntakes) {
+	Intake.find({}, function(err, foundIntakes) {
 		if (err) { // error ?
 			res.send(err);
 		}
 		res.json(foundIntakes); // return all intakes in json
 	});
-
-
 });
 
 // GET single
-server.get('/intake/:caseNumber', function (req, res, next) {
+server.get('/intake/:caseNumber', function(req, res, next) {
 
 	// Intake.findOne({
 	// 	_id: req.params.id
 	Intake.findOne({
 		caseNumber: req.params.caseNumber
-	}, function (err, intake) {
+	}, function(err, intake) {
 		if (err) {
 			return next(new restify.InvalidArgumentError(
 				JSON.stringify(err.errors)));
 		}
 		if (intake) {
 			res.send(intake);
-		} else {
+		}
+		else {
 			res.send(404);
 		}
 	});
 });
 
 // POST single
-server.post('/intake', function (req, res, next) {
+server.post('/intake', function(req, res, next) {
 	// console.log(req.params);
 	if (req.params.taker === undefined) {
 		console.log('name is undefined yo');
@@ -88,7 +83,7 @@ server.post('/intake', function (req, res, next) {
 
 	Intake.create({
 		taker: req.params.taker
-	}, function (err, intake) {
+	}, function(err, intake) {
 		if (err) {
 			return next(
 				new restify.InvalidArgumentError(JSON.stringify(err.errors))
@@ -99,35 +94,57 @@ server.post('/intake', function (req, res, next) {
 
 });
 
-server.listen(9000, function () {
+// DELETE single
+server.del('/intake/:caseNumber', function(req,res,next){
+	Intake.remove({
+		caseNumber: req.params.caseNumber
+	}, function(err, deletedIntake){
+		if(err) {
+			return next(
+				new restify.InvalidArgumentError(JSON.stringify(err.errors))
+			);
+		}
+		res.send(200, 'Deleted intake with caseNumber of :' + deletedIntake);
+	});
+});
+
+server.listen(9000, function() {
 	console.log('%s Listening at %s', server.name, server.url);
 });
 
-testIntakeInsert = function () {
+testIntakeInsert = function() {
 	var _data = {
-		taker:  'david zzzz',
-		contributorType : 'Volunteer',
-		caseNumber: 5,
-		hidden: true,
-		callbackNeeded : true,
-		caseType: 'H',
-		caller : {
-			name : {
-				first : 'david',
-				last : 'zzzzz'
+		'taker': 'david zzzz',
+		'contributorType': 'Volunteer',
+		'caseNumber': 1,
+		'hidden': true,
+		'callbackNeeded': true,
+		'caseType': 'H',
+		'caller': {
+			'name': {
+				'first': 'david',
+				'last': 'zzzzz'
 			},
-			email :  'user@msn.com',
-			address: '123 wash st'
+			'email': 'user@msn.com',
+			'address': '123 wash st'
 		},
-		callerPresentsAs : 'Familiy',
-		referedBy : 'Familiy'
+		'callerPresentsAs': 'Familiy',
+		'referedBy': 'Familiy'
 	};
 
-	return new Intake(_data).save(function (err, savedIntake) {
-		if (err) {
-			throw err;
-		}
-		console.log('test intake001 was successful'.green);
-		console.dir(savedIntake);
-	});
+	return new Intake(_data)
+		.save(function(err, savedIntake) {
+			if (err) {
+				throw err;
+			}
+			console.log('test intake001 was successful'.green);
+			console.dir(savedIntake);
+		});
 };
+
+// http://stackoverflow.com/questions/5535610/mongoose-unique-index-not-working
+Intake.on('index', function(err) {
+	if (err) {
+		console.error(err);
+	}
+});
--- models/Intake.js
+++ models/Intake.js
@@ -1,66 +1,79 @@
 /*====================================
 
-	TODO:
-	[   ] - refactor all that requires a radio input selection
-	[--] - refactor caller
-	[X] - refactor taker /  user
-	[   ] - replace date with objectid timestamp iso created time
-	[X] - setup get one by $index
+		TODO:
+		[X] - refactor taker /  user
+		[X] - setup get one by $index
+		[   ] - refactor all that requires a radio input selection
+		[   ] - replace date with objectid timestamp iso created time
+		[--] - drop caseNumber index multiples
+		[	] - handle err for duplicate caseNumber
 
 ====================================*/
 
 
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId;
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
 
 var IntakeSchema = new Schema({
 
-	// 1a, unique random incremented {basedon : process id, time }
-	id: ObjectId,
-	// 1b
-	taker:String,
-	contributorType: String,
-	// 2a
-	// replace with objectid timestamp iso created time
-	date: { type: Date, default: Date.now},
-	// 3, index
-	caseNumber: { type : Number, index: {unique:true} },
-	hidden: { type: Boolean, default: false},
-	// 2b
-	callbackNeeded: Boolean,
-	// 4
-	caseType: String, // radio input
-	// 5
-	caller: {
-		name: {
-			first: String,
-			last: String
-		},
-		address : String,
-		county : String,
-		phone: {
-			number : Number,
-			// ok to call ?
-			private : Boolean
-		},
-		// alternative phone number
-		altPhone: {
-			number : Number,
-			// ok to call ?
-			private : Boolean
-		},
-		// email: {type: string, unique:false},
-		email: String,
+    // 1a, unique random incremented {basedon : process id, time }
+    id: ObjectId,
+    // 1b
+    taker: String,
+    contributorType: String,
+    // 2a
+    // replace with objectid timestamp iso created time
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    // 3, index
+    caseNumber: {
+        type: Number,
+        index: {
+            unique: true,
+            dropDups: true
+        }
+    },
+    hidden: {
+        type: Boolean,
+        default: false
+    },
+    // 2b
+    callbackNeeded: Boolean,
+    // 4
+    caseType: String, // radio input
+    // 5
+    caller: {
+        name: {
+            first: String,
+            last: String
+        },
+        address: String,
+        county: String,
+        phone: {
+            number: Number,
+            // ok to call ?
+            private: Boolean
+        },
+        // alternative phone number
+        altPhone: {
+            number: Number,
+            // ok to call ?
+            private: Boolean
+        },
+        // email: {type: string, unique:false},
+        email: String,
 
-	},
-	// 6
-	callerPresentsAs: String, // radio input
-	// 7
-	callerAssessedAs: String, // radio input
-	// 8
-	referedBy: String // radio input
+    },
+    // 6
+    callerPresentsAs: String, // radio input
+    // 7
+    callerAssessedAs: String, // radio input
+    // 8
+    referedBy: String // radio input
 });
 
 // 1 means ascending order..
